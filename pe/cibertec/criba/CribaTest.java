1c44f4b908ab2f2df14d04a162224ad6a3bc50c8
jmejiarojas <julioedgarmejiarojas338@gmail.com>
2016-08-27 00:38:33 -0500

    Pimera refactorizacion
    Parece evidente que nuestro método generarPrimos realiza tres
    funciones diferentes, por lo que de generarPrimos extraemos tres
    métodos diferentes. Además, buscamos un nombre más adecuado para
    la clase y eliminamos todos los comentarios innecesarios.

--- pe/cibertec/criba/CribaTest.java
+++ pe/cibertec/criba/CribaTest.java
@@ -9,19 +9,22 @@ public class CribaTest {
 	@Test
 	public void generarPrimosTest() {
 		
-		int[] nullArray = Criba.generarPrimos(0);
+		int[] nullArray = GeneradorDePrimos.generarPrimos(0);
 		assertEquals(nullArray.length, 0);
+		
+		int[] arrayVacio = GeneradorDePrimos.generarPrimos(1);
+		assertEquals(arrayVacio.length, 0);
 
-		int[] minArray = Criba.generarPrimos(2);
+		int[] minArray = GeneradorDePrimos.generarPrimos(2);
 		assertEquals(minArray.length, 1);
 		assertEquals(minArray[0], 2);
 
-		int[] threeArray = Criba.generarPrimos(3);
+		int[] threeArray = GeneradorDePrimos.generarPrimos(3);
 		assertEquals(threeArray.length, 2);
 		assertEquals(threeArray[0], 2);
 		assertEquals(threeArray[1], 3);
 		
-		int[] centArray = Criba.generarPrimos(100);
+		int[] centArray = GeneradorDePrimos.generarPrimos(100);
 		assertEquals(centArray.length, 25);
 		assertEquals(centArray[24], 97);
 	}
